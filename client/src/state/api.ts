2bed15c620f96f7aa70277c91d2a1aa1b6e07d69
Sevugan Chetty Kathiresan <sevugankathiresan@gmail.com>
2024-02-07 07:17:40 +1100

    Row 2 charts completed

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,18 +1,22 @@
 import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse } from "./types";
 
 // This file is concerned with creating a creating a template engine which lets us write all the API calls we need to make for our app to run
 
 export const api = createApi({
     baseQuery:fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void>({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"],
         }),
+        getProducts: build.query<Array<GetProductsResponse>, void>({
+            query: () => "product/products/",
+            providesTags: ["Products"],
+        }),
     }),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
